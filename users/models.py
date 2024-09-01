8a20deaf4945d2e47776bc417d7c6383dbf7e453
KPACABA1 <groninger.official@yandex.ru>
2024-09-01 05:22:41 +0500

    споткнулся на верификации почты

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
     """Модель пользователя, где изменено имя пользователя на почту. Так же реализована верификация пользователя по почте"""
     username = None
     email = models.EmailField(unique=True, verbose_name='Почта пользователя')
+    token = models.CharField(max_length=60, verbose_name='Токен для авторизации', null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -14,6 +15,10 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        permissions = [
+            ('may_block_users', 'Может заблокировать пользователя'),
+            ('can_view_list_of_users', 'Может просматривать список пользователей сервиса')
+        ]
 
     def __str__(self):
         return self.email
