507bad1231b8367446ba666885ac9bc8c969bc6e
KPACABA1 <groninger.official@yandex.ru>
2024-09-01 05:21:51 +0500

    споткнулся на верификации почты

--- mailing/views.py
+++ mailing/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
-from mailing.forms import MessageForm, CustomerForm, MailingForm
+from mailing.forms import MessageForm, CustomerForm, MailingForm, MailingManagerForm
 from mailing.models import Message, Customer, Mailing, Attempt
 
 
@@ -13,12 +14,20 @@ class MessageListView(ListView):
     template_name = 'mailing/message_list.html'
 
 
-class MessageCreateView(CreateView):
+class MessageCreateView(LoginRequiredMixin, CreateView):
     """Класс контроллер для создания сообщений для рассылки"""
     model = Message
     form_class = MessageForm
     success_url = reverse_lazy('mailing:message_list')
     template_name = 'mailing/message_form.html'
+
+    def form_valid(self, form):
+        """Метод для добавления к сообщениям рассылок их создателей"""
+        message = form.save()
+        user = self.request.user
+        message.creator = user
+        message.save()
+        return super().form_valid(form)
 
 
 class MessageUpdateView(UpdateView):
@@ -45,11 +54,19 @@ class CustomerListView(ListView):
     model = Customer
 
 
-class CustomerCreateView(CreateView):
+class CustomerCreateView(LoginRequiredMixin, CreateView):
     """Класс контроллер для создания клиентов сервиса"""
     model = Customer
     form_class = CustomerForm
     success_url = reverse_lazy('mailing:customer_list')
+
+    def form_valid(self, form):
+        """Метод для добавления к клиенту сервиса его создателя"""
+        customer = form.save()
+        user = self.request.user
+        customer.creator = user
+        customer.save()
+        return super().form_valid(form)
 
 
 class CustomerUpdateView(UpdateView):
@@ -75,18 +92,34 @@ class MailingListView(ListView):
     model = Mailing
 
 
-class MailingCreateView(CreateView):
+class MailingCreateView(LoginRequiredMixin, CreateView):
     """Класс контроллер для создания рассылок"""
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy('mailing:mailing_list')
 
+    def form_valid(self, form):
+        """Метод для добавления к рассылке её создателя"""
+        mailing = form.save()
+        user = self.request.user
+        mailing.creator = user
+        mailing.save()
+        return super().form_valid(form)
 
-class MailingUpdateView(UpdateView):
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
     """Класс контролер для редактирования рассылок"""
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy('mailing:mailing_list')
+
+    def get_form_class(self):
+        """В зависимости от роли пользователя будет выводиться определенная форма"""
+        user = self.request.user
+        if user.has_perm('mailing.can_disable_mailing'):
+            return MailingManagerForm
+        return MailingForm
+
 
 
 class MailingDetailView(DetailView):
